def7f1cd83a5e3ca0927d6781d410ce375ca9dbc
Tyler Collett <tyler.quinn.collett@gmail.com>
2025-09-17 19:27:48 -0600

    Hamburger Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 // inject current year
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const hamMenu = document.querySelector('#hamburger');
+const navShow = document.querySelector('#navWrapper')
+
+hamMenu.addEventListener('click', () => {
+    if (hamMenu.className === "hide"){
+        hamMenu.className = "show"
+        hamMenu.src = "img/close.png"
+        navShow.className = "show"
+    }
+    else{
+        hamMenu.className = "hide"
+        hamMenu.src = "img/hamburger.png"
+        navShow.className = "hide"
+    }
+})
