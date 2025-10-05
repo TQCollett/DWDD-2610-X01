29dfd1ab8e3680c9e6a40d3b5441c8ce32885dc7
Tyler Collett <tyler.quinn.collett@gmail.com>
2025-10-05 16:18:20 -0600

    reviews

---
+++ data/reviews.js
@@ -0,0 +1,27 @@
+export const reviews =[
+    {
+        "name": "Joe S. Schmo",
+        "stars": 5,
+        "text": "Great selection of healthy fish and quality supplies. Staff were friendly and gave helpful advice. Highly recommend for any aquarium enthusiast!",
+    },
+    {
+        "name": "Karen T. Much",
+        "stars": 1,
+        "text": "Poor selection, several fish looked unhealthy, and fish were too wet. Staff seemed uninterested and not very helpful. Won't be returning."
+    },
+    {
+        "name": "Chad R. Smith",
+        "stars": 4,
+        "text": "The store has a fair selection of fish and supplies. Some tanks were well-maintained, others not so much. Staff were okay, but not very engaged. Not bad overall, but could be better."
+    },
+    {
+        "name": "Timmy Tommy",
+        "stars": 5,
+        "text": "Wide variety of healthy fish and clean tanks. Staff were knowledgeable and gave great care tips. Prices were reasonable too. Will definitely shop here again!"
+    },
+    {
+        "name": "Jimmy Jones",
+        "stars": 2,
+        "text": "Some fish looked stressed and tanks weren't very clean. Limited stock and staff didn'’'t seem interested in helping. Expected better for the prices."
+    }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // inject current year
+
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
 
@@ -18,3 +19,33 @@ hamMenu.addEventListener('click', () => {
         navShow.className = "hide"
     }
 })
+
+//Review Card Generation Maintenance Checklist
+import {reviews} from "../data/reviews.js"
+console.log(reviews)
+
+const myReviews = document.querySelector('#reviews')
+
+for (let x = 0; x < reviews.length; x++){
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+    
+    const myRating = document.createElement('div')
+
+
+
+    for(let step = 0; step<reviews[x].stars; step++){
+        const genIndicator = document.createElement ('img')
+        genIndicator.src= "../media/svg/star.svg"
+        myRating.appendChild(genIndicator)
+    }
+
+    mySection.appendChild(myName)
+    mySection.appendChild(myRating)
+    mySection.appendChild(myReview)
+
+    myReviews.appendChild(mySection)
+}
